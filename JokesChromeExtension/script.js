640eeb60acbb3dd5340b5825c6a3ba0d0e7843bb
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-10-17 16:41:24 +0530

    Changing the Color of the Font.

--- JokesChromeExtension/script.js
+++
@@ -1,9 +0,0 @@
-fetch(
-  'https://icanhazdadjoke.com/slack')
-  .then(data=>data.json())
-  .then(jokeData=> {
-    const jokeText=jokeData.attachements[0].text;
-    const jokeElement=document.getElementById('jokeElement')
-
-    jokeElement.innerHTML=jokeText;
-  })
